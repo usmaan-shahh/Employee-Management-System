fc9b4efbbab70c538995104202e9aba94543d554
usmaan <usi.shah06@gmail.com>
2025-02-02 15:33:18 +0530

    feat: add completed field to task model and implement error handling in task creation route

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -4,6 +4,7 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   dueDate: { type: Date },
+  completed: { type: String, default: false },
   priority: { type: String, enum: ["low", "medium", "high"], default: "low" },
 });
 const Task = mongoose.model("Task", taskSchema);
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,14 +2,20 @@ import express from "express";
 import Task from "../models/taskModel.js";
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { title, description, dueDate, priority } = req.body;
   const newTask = new Task({
-    title,
-    description,
-    dueDate,
-    priority,
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    priority: priority,
   });
+  try {
+    const savedTask = await newTask.save();
+    res.status(200).json(savedTask);
+  } catch (error) {
+    res.status(404).json({ message: "task creation failed" });
+  }
 });
 
 export default router;
