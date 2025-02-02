c41793c760cced9e5bc0a6e2528f6d6efde0a41c
usmaan <usi.shah06@gmail.com>
2025-02-02 18:28:38 +0530

    feat: implement GET and PUT routes for tasks; include completed field in task model

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,19 +2,53 @@ import express from "express";
 import Task from "../models/taskModel.js";
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(404).json({ message: "task reading failed" });
+  }
+});
+
 router.post("/", async (req, res) => {
-  const { title, description, dueDate, priority } = req.body;
+  const { title, description, completed, dueDate, priority } = req.body;
   const newTask = new Task({
     title,
     description,
     dueDate,
-    priority: priority,
+    priority,
+    completed,
   });
   try {
     const savedTask = await newTask.save();
     res.status(200).json(savedTask);
   } catch (error) {
     res.status(404).json({ message: "task creation failed" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, dueDate, priority, completed } = req.body;
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        dueDate,
+        priority,
+        completed,
+      },
+      { new: true }
+    );
+    if (!updatedTask) {
+      res.status(404).json({ message: "task not found" });
+    }
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    res.status(404).json({ message: "task updation failed" });
   }
 });
 
