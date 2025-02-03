adfd5f5fda9677dcaaa1d83049b75b5ec238b96c
usmaan <usi.shah06@gmail.com>
2025-02-03 19:20:27 +0530

    feat: migrate to Redux Toolkit for task management and remove FormPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,7 @@
-import React, { useEffect, useState } from "react";
-import FormPage from "./components/FormPage";
+import React from "react";
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
-
-  const fetchAllTasks = async () => {
-    try {
-      const response = await fetch("http://localhost:8000/tasks");
-      if (!response.ok) {
-        throw new Error("Failed to fetch tasks");
-      }
-      const allTasks = await response.json();
-      setTasks(allTasks);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  useEffect(() => {
-    fetchAllTasks();
-  }, []);
-  return (
-    <div>
-      <h1>Todo App</h1>
-      <FormPage fetchAllTasks={fetchAllTasks} />
-      {tasks.map((task, index) => (
-        <div key={index}>
-          <h4>
-            Title: <u>{task.title}</u>
-          </h4>
-          <h4>
-            Description: <u>{task.description}</u>
-          </h4>
-          <h4>
-            Due Date: <u>{task.dueDate}</u>
-          </h4>
-          <h4>
-            Completed: <u>{task.completed ? "Yes" : "No"}</u>
-          </h4>
-          <h4>
-            Priority:<u>{task.priority}</u>
-          </h4>
-        </div>
-      ))}
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
---
+++ frontend/src/api/todoApi.js
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const todoApi = createApi({
+  reducerPath: "todoApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => "/tasks",
+      //   providesTags: ["Todos"],
+    }),
+    addTodo: builder.mutation({
+      query: (formData) => ({
+        url: "/todos",
+        method: "POST",
+        body: formData,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+    deleteTodo: builder.mutation({
+      query: (id) => ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+  }),
+});
+
+export const { useGetTodosQuery, useAddTodoMutation, useDeleteTodoMutation } =
+  todoApi;
