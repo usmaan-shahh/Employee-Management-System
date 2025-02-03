17ef317bf41d453f37debf13d0c299d8d0009ff9
usmaan <usi.shah06@gmail.com>
2025-02-03 23:12:23 +0530

    feat: enhance TodoForm to manage form state and dispatch addTodo action

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { addTodo } from "../slice/todoSlice";
+import { useDispatch } from "react-redux";
 const TodoForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    age: "",
+    describeYourself: "",
+  });
+  const dispatch = useDispatch();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addTodo(formData));
+    setFormData({
+      name: "",
+      email: "",
+      age: "",
+      describeYourself: "",
+    });
+  };
   return (
     <div>
       <h1>Todo App</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Enter Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleChange} />
 
         <label>Enter E-mail</label>
-        <input type="text" name="email" />
+        <input type="text" name="email" onChange={handleChange} />
 
         <label>Age</label>
-        <input type="text" name="age" />
+        <input type="text" name="age" onChange={handleChange} />
 
         <label>Describe Yourself</label>
-        <textarea name="describeYourself" />
+        <textarea name="describeYourself" onChange={handleChange} />
 
         <button type="submit">Add Data</button>
       </form>
