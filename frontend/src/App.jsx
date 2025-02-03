9d93549799dc41db5fd83fdb4df92998b01c7ba3
usmaan <usi.shah06@gmail.com>
2025-02-03 19:50:47 +0530

    feat: add TodoForm component and integrate with TodoList for task management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TodoForm from "./components/TodoForm";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <TodoForm />
+    </div>
+  );
 };
 
 export default App;
