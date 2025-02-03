5b373b585248af84f7f7d1980865800de73ab825
usmaan <usi.shah06@gmail.com>
2025-02-04 01:05:02 +0530

    feat: enhance Todo API and components with tag support and refetch capability

--- frontend/src/api/todoApi.js
+++ frontend/src/api/todoApi.js
@@ -3,16 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const todoApi = createApi({
   reducerPath: "todoApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
+  tagTypes: ["Todos"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/tasks",
+      providesTags: ["Todos"],
     }),
     addTodos: builder.mutation({
       query: (formData) => ({
         url: "/tasks",
         method: "POST",
         body: formData,
-        // headers: { "Content-Type": "application/json" },
       }),
       invalidatesTags: ["Todos"],
     }),
