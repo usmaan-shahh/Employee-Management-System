6f9d0cdce22bc649fe6dd86312c9e6c34bf4fca7
usmaan <usi.shah06@gmail.com>
2025-02-03 22:14:39 +0530

    feat: enhance App component to display loading and error states, and render todos with detailed information

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,35 @@ import React from "react";
 import TodoForm from "./components/TodoForm";
 import { useGetTodosQuery } from "./api/todoApi";
 const App = () => {
-  const { data: todos } = useGetTodosQuery();
+  const { data: todos, isLoading, error } = useGetTodosQuery();
+  if (isLoading) return <p>Loading todos...</p>;
+  if (error) return <p>Error fetching todos</p>;
   console.log(todos);
   return (
     <div>
       <TodoForm />
+      {todos.map((todo) => (
+        <ul>
+          <hr />
+          <li>
+            <b>Name: </b>
+            {todo.name}
+          </li>
+          <li>
+            <b>Email: </b>
+            {todo.email}
+          </li>
+          <li>
+            <b>Age: </b>
+            {todo.age}
+          </li>
+          <li>
+            <b>Describe Yourself: </b>
+            {todo.describeYourself}
+          </li>
+          <hr />
+        </ul>
+      ))}
     </div>
   );
 };
