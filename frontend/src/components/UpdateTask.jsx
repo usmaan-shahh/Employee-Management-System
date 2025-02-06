0a69eb1f1095021fe8e2ba0a6408feb06295d63e
usmaan <usi.shah06@gmail.com>
2025-02-07 01:09:37 +0530

    refactor: remove Modal component and replace with alert notifications in UpdateTask component

--- frontend/src/components/UpdateTask.jsx
+++ frontend/src/components/UpdateTask.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useUpdateTaskMutation } from "../api/todoApi";
 import { useDispatch, useSelector } from "react-redux";
-import Modal from "./Modal";
 import { setFieldValue, editFieldValue, resetForm } from "../slices/formSlice";
 
 const UpdateTask = ({ todo, closeForm }) => {
   const dispatch = useDispatch();
   const formData = useSelector((state) => state.form);
-  const [modalMessage, setModalMessage] = useState("");
-  const [isUpdateSuccess, setIsUpdateSuccess] = useState(false);
-  const [updateTask, { isLoading }] = useUpdateTaskMutation();
+  const [updateTask, { isLoading, isError, isSuccess }] =
+    useUpdateTaskMutation();
 
   useEffect(() => {
     dispatch(editFieldValue(todo));
@@ -24,21 +22,12 @@ const UpdateTask = ({ todo, closeForm }) => {
     try {
       await updateTask({ id: todo._id, data: formData });
       dispatch(resetForm());
-      setModalMessage("Task updated successfully!");
-      setIsUpdateSuccess(true);
+      closeForm();
     } catch (error) {
       console.error("Error updating:", error);
-      setModalMessage("Error updating task.");
-      setIsUpdateSuccess(false);
     }
   };
-  const closeModal = () => {
-    setModalMessage("");
-    if (isUpdateSuccess) {
-      closeForm();
-    }
-    setIsUpdateSuccess(false);
-  };
+
   return (
     <div className="p-6 border-t border-gray-300 bg-white shadow-md rounded-lg">
       <h3 className="text-xl font-semibold text-gray-800 mb-4">Update Task</h3>
@@ -55,6 +44,7 @@ const UpdateTask = ({ todo, closeForm }) => {
             placeholder="Enter your name"
           />
         </div>
+
         <div className="flex flex-col">
           <label className="text-gray-700 font-medium">Email:</label>
           <input
@@ -67,6 +57,7 @@ const UpdateTask = ({ todo, closeForm }) => {
             placeholder="Enter your email"
           />
         </div>
+
         <div className="flex flex-col">
           <label className="text-gray-700 font-medium">Age:</label>
           <input
@@ -79,6 +70,7 @@ const UpdateTask = ({ todo, closeForm }) => {
             placeholder="Enter your age"
           />
         </div>
+
         <div className="flex flex-col">
           <label className="text-gray-700 font-medium">
             Describe Yourself in one word:
@@ -93,6 +85,7 @@ const UpdateTask = ({ todo, closeForm }) => {
             placeholder="Enter your age"
           />
         </div>
+
         <div className="flex gap-3">
           <button
             type="submit"
@@ -109,7 +102,8 @@ const UpdateTask = ({ todo, closeForm }) => {
             Cancel
           </button>
         </div>
-        {modalMessage && <Modal message={modalMessage} onClose={closeModal} />}
+        {isSuccess && alert("Task Updated Successfully")}
+        {isError && alert("Error updating task")}
       </form>
     </div>
   );
