7b3c42c9f20b0d8145add9017d2b2cfaf507a9ff
usmaan <usi.shah06@gmail.com>
2025-02-10 21:44:18 +0530

    refactor: enable CORS with specific options for improved security

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,17 @@ import express from "express";
 import connectDB from "./configuration/mongoDb.js";
 import dotenv from "dotenv";
 import cors from "cors";
-// const corsOption = {
-//   origin: "https://todo-1-w7yl.onrender.com",
-//   credentials: true,
-// };
+const corsOption = {
+  origin: "https://todo-1-w7yl.onrender.com",
+  credentials: true,
+};
 import path from "path";
 dotenv.config();
 import router from "./routes/tasks.js";
 connectDB();
 const _dirname = path.resolve();
 const app = express();
-app.use(cors());
+app.use(cors(corsOption));
 app.use(express.json());
 app.use("/tasks", router);
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
